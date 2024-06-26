f45851441c4909fb636b3bbf8144d6b7b9a23107
Ezra Gubbay <ezragubbay99@gmail.com>
2024-06-26 20:41:24 +0300

    Delete src directory

--- src/MissingVariableMappingException.java
+++
@@ -1,7 +0,0 @@
-public class MissingVariableMappingException extends Exception {
-
-    public MissingVariableMappingException() {
-
-        super("Missing variable assignment - assignment is required to evaluate the expression.");
-    }
-}
